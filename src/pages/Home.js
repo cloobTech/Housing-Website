ed7a0ef46590e25853e41c4de4211deb6a228e77
cloobTech <belkid98@gmail.com>
2021-11-18 05:33:28 +0100

    T

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ const Home = ({ children, title, description, address }) => {
         <div className={home.title}>
           <h1>CLOOB HOMES</h1>
           <p>...your number one housing agency</p>
+          <p className="">Hello world</p>
         </div>
       </div>
       <Body></Body>
